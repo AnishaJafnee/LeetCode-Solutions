f80a5ec541b20941576215b463057076d7cfebef
AnishaJafnee <24csa10@karpagamtech.ac.in>
2026-04-02 09:52:43 +0530

    Time: 0 ms (100%), Space: 7.8 MB (64.08%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n) {
+        int f1=0,f2=1;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        
+        for(int i=2;i<n;i++)
+        {
+            int t=f1;
+            
+            f1=f2;
+            f2=t+f1;
+        }
+        return f1+f2;
+    }
+};
